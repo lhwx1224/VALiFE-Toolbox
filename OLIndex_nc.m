4c72d154bd2104327986f91d8f2b6ff7fea56caf
He-Wen-Xuan (Vincent) Li <hewenxuan_li@uri.edu>
2022-03-17 21:20:57 -0400

    Updated

--- OLIndex_nc.m
+++ OLIndex_nc.m
@@ -1,4 +1,4 @@
-function OLI = OLIndex_nc(Omega)
+function OLI = OLIndex_nc(Omega, rol)
 % OLIndex_nc returns the identified overloads from the overload candidate
 % matrix Omega. 
 %
@@ -13,6 +13,9 @@ function OLI = OLIndex_nc(Omega)
 
 [m, ~] = size(Omega);
 % omegam = Omega(m,:);
+
+%% ======================== FORWARD RUN ================================
+
 % ------------------------------------------------------------------------
 %                ROW-WISE WEIGHT THE CANDIDIATE MATRIX
 % ------------------------------------------------------------------------
@@ -38,3 +41,28 @@ oli = dOmega_s < 0;
 %                         OVERLOAD INDICES MATRIX
 % ------------------------------------------------------------------------
 OLI = Omega.*[double(oli) 0];
+
+%% ======================= BACKWARD SIFT ===============================
+
+% ------------------------------------------------------------------------
+%                ROW-WISE WEIGHT THE CANDIDIATE MATRIX
+% ------------------------------------------------------------------------
+Omega_w = OLI;
+
+% ------------------------------------------------------------------------
+%                CONDUCT ROW-WISE SUMMATION: Omega_s
+% ------------------------------------------------------------------------
+Omega_s = sum(Omega_w, 1);
+Omega_s = [1:length(Omega_s); Omega_s];
+Omega_s_ol = Omega_s(1,Omega_s(2,:) == 1);
+
+% ------------------------------------------------------------------------
+%         GET DIFFERENCE OF THE TWO CONSECUTIVE WEIGHTED CANDIDATES
+% ------------------------------------------------------------------------
+dOmega_s_ol = Omega_s_ol(:,2:end) - Omega_s_ol(:,1:end-1);
+dOmega_s_ol = [nan(1,rol), dOmega_s_ol];
+
+neg_indx = Omega_s_ol(1,dOmega_s_ol <= rol);
+
+OLI(:, neg_indx) = zeros(size(OLI,1),length(neg_indx));
+
