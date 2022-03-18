98a1bba7d27c485c38ac4f67e0b0cdfabb054454
He-Wen-Xuan (Vincent) Li <hewenxuan_li@uri.edu>
2022-03-18 09:58:58 -0400

    Updated RandomOL1.m

--- RandomOL1.m
+++ RandomOL1.m
@@ -1,4 +1,4 @@
-function [t, ] = RandomOL1()
+function [t, s_rol] = RandomOL1(s_min, s_baseline, N, ols_ini, OLR, NOL, InitTime, FinalTime)
 % RandomOL1 returns a reversal vector by designating a based line loading,
 % the overload ratio, and the period of overloads.
 %
@@ -7,9 +7,9 @@ function [t, ] = RandomOL1()
 % input: s_min - minimum stress of the baseline loading
 %        s_baseline - maximum stress of the baseline loading
 %        N - total number of cycles included in the CAL
-%        ols_init - the appearance of the first overload
-%        ols_T - overoad period
+%        ols_ini - the appearance of the first overload
 %        OLR - overload ratio
+%        NOL - total number of overloads
 %        InitTime - initial time stamp
 %        FinalTime - ending time stamp
 %
@@ -20,7 +20,7 @@ function [t, ] = RandomOL1()
 % Created on March 18, 2022
 
 % ------------- INQUIRE THE INPUT ---------------------------------------
-if nargin < 7
+if nargin < 8
     InitTime = 1;
     FinalTime = N;
 end
@@ -29,20 +29,16 @@ end
 [t,s_CAL]=CAL(s_min,s_baseline, N, InitTime, FinalTime);
 % -----------------------------------------------------------------------
 
-%------ create a random overload statistics ------------
-% ols_ini = 1; % the initial location of the ols to appear
+%-------------- CREATE A RANDOM OVERLOAD STATISTICS ---------------------
 ols_end = numel(t)-1;
 % the indexes for ols is multiply by 2 because the odd locations is set for 
 % minimum stress value and even location for maximum stress value
 list_ols = ols_ini*2:2:ols_end;
-num_ols = 5; % number of ols to occur in the stress history
 % randomly permuted the number from the range of ols
-rand_peaks = randperm(numel(list_ols),num_ols);
+rand_peaks = randperm(numel(list_ols),NOL);
 ols_indx = list_ols(rand_peaks);
-stress_ols = stress_array;
-ols_ratio = 1.5;% the stress ratio = stress_max/stress_base_line
+s_rol = s_CAL;
 % the stress overload 
-stress_ols(ols_indx) = sigma_baseline*ols_ratio;
-% stress_ols(end-1) = sigma_baseline;
-
+s_rol(ols_indx) = s_baseline*OLR;
+% -----------------------------------------------------------------------
 return
